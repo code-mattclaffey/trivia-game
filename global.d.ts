ae0b97afbe6b72b1c7ef3f25bce1dcbc8118ca2d
code-mattclaffey <mattclaffey93@gmail.com>
2020-10-22 16:46:57 +0100

    chore: move provider in firebase wrapper

--- global.d.ts
+++ global.d.ts
@@ -7,7 +7,8 @@ enum GameStatuses {
 interface Player {
   id: string;
   name: string;
-  score: number;
+  correctAnswers: number;
+  incorrectAnswers: number;
 }
 
 interface ScoreBoard {
@@ -34,21 +35,15 @@ interface OpenTbdApiResponse {
 
 interface Game {
   id: string;
-  status: keyof GameStatuses; // to change state of page when status changes
+  status: keyof typeof GameStatuses; // to change state of page when status changes
+  currentQuestionId: string;
   questionStage: number; // to determine which question the user is on
-  questions: Array<Question>;
+  questions?: Array<Question>;
+  players?: Array<Player>;
 }
 
 interface FirebaseState {
   games: {
-    [quid: string]: {
-      // game id
-      id: string; // game id
-      status: keyof GameStatuses;
-      questionStage: number;
-      currentQuestionId: string;
-      questions: Array<Question>;
-      players: Array<Player>;
-    };
+    [quid: string]: Game;
   };
 }
--- src/components/firebase-wrapper/firebase-wrapper.component.tsx
+++ src/components/firebase-wrapper/firebase-wrapper.component.tsx
@@ -1,18 +1,58 @@
-import React from "react";
-import { useFirebase } from "../../hooks/firebase";
+import React, { createContext, useContext, useEffect, useState } from "react";
+import firebase from "firebase/app";
+import "firebase/database";
+import firebaseConfig from "./config";
+
+export const defaultGameState: Game = {
+  id: "",
+  status: "NOT_STARTED",
+  currentQuestionId: "",
+  questionStage: 0,
+  questions: [],
+  players: [],
+};
+
+export const FirebaseWrapperContext = createContext<Game>(defaultGameState);
+
+export const useFirebaseWrapper = () => useContext(FirebaseWrapperContext);
 
 const FirebaseWrapper: React.FC = ({ children }) => {
-  const { data, loading, error } = useFirebase();
+  const [app, setFirebaseInstance] = useState<{ apps: Array<any> }>();
+  const [data, setData] = useState<Game>(defaultGameState);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const storage = localStorage.getItem("trivia-state");
+
+    if (storage) {
+      setData(JSON.parse(storage));
+      setLoading(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (!app?.apps.length) {
+      firebase.initializeApp(firebaseConfig);
+
+      setFirebaseInstance(firebase);
+
+      firebase
+        .database()
+        .ref("trivia")
+        .on("value", (snapshot) => {
+          setData(snapshot.val());
+          localStorage.setItem("trivia-state", JSON.stringify(snapshot.val()));
+          setLoading(false);
+        });
+    }
+  }, [app]);
 
   if (loading) return <p>Loading</p>;
-  if (error) return <p>Error</p>;
-  if (!data) return null;
 
   return (
-    <>
-      <p>{data.test}</p>
+    <FirebaseWrapperContext.Provider value={data}>
       {children}
-    </>
+    </FirebaseWrapperContext.Provider>
   );
 };
 
