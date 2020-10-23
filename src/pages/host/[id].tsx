4b778b8c2398a2d7fe8eb4fd1598de97714e288b
code-mattclaffey <mattclaffey93@gmail.com>
2020-10-23 13:51:15 +0100

    feat: add share link using web share api

--- src/pages/host/[id].tsx
+++ src/pages/host/[id].tsx
@@ -2,20 +2,32 @@ import React from "react";
 import Layout from "../../components/Layout";
 import FirebaseWrapper from "../../components/firebase-wrapper";
 import { useRouter } from "next/router";
-import Link from "next/link";
 
 const IndexPage: React.FC = () => {
   const router = useRouter();
   const { id } = router.query;
   const gameId: any = id;
 
+  const onClick = (url: string) => {
+    if (navigator.share) {
+      navigator
+        .share({
+          title: "Quiz time!",
+          url: url,
+        })
+        .catch(console.error);
+    } else {
+      // fallback
+    }
+  };
+
   return (
     <FirebaseWrapper gameId={gameId}>
       <Layout title="Host">
         <h1>Host</h1>
-        <Link href={`/join/${id}`}>
-          <a>Share link with friends</a>
-        </Link>
+        <button onClick={() => onClick(`/join/${id}`)}>
+          Share link with friends
+        </button>
       </Layout>
     </FirebaseWrapper>
   );
