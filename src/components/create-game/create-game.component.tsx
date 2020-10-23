95459c543dbf1b26e3dc07ebc4098a19c97fc897
code-mattclaffey <mattclaffey93@gmail.com>
2020-10-23 14:41:20 +0100

    chore: tidy up create game component

--- src/components/create-game/create-game.component.tsx
+++ src/components/create-game/create-game.component.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useFirebaseWrapper } from "../firebase-wrapper/firebase-wrapper.component";
 import { withTypes, Field } from "react-final-form";
+import { categories, difficulties, types } from "./form-values";
 
 const buildTrivaUrl = ({
   amount,
@@ -54,10 +55,9 @@ const CreateGame = () => {
                 component="select"
                 placeholder="Amount"
               >
-                <option value="any">Any Difficulty</option>
-                <option value="easy">Easy</option>
-                <option value="medium">Medium</option>
-                <option value="hard">Hard</option>
+                {difficulties.map(({ label, value }) => (
+                  <option value={value}>{label}</option>
+                ))}
               </Field>
 
               <label htmlFor="category">Category</label>
@@ -67,44 +67,16 @@ const CreateGame = () => {
                 component="select"
                 placeholder="Amount"
               >
-                <option value="any">Any Category</option>
-                <option value="9">General Knowledge</option>
-                <option value="10">Entertainment: Books</option>
-                <option value="11">Entertainment: Film</option>
-                <option value="12">Entertainment: Music</option>
-                <option value="13">
-                  Entertainment: Musicals &amp; Theatres
-                </option>
-                <option value="14">Entertainment: Television</option>
-                <option value="15">Entertainment: Video Games</option>
-                <option value="16">Entertainment: Board Games</option>
-                <option value="17">Science &amp; Nature</option>
-                <option value="18">Science: Computers</option>
-                <option value="19">Science: Mathematics</option>
-                <option value="20">Mythology</option>
-                <option value="21">Sports</option>
-                <option value="22">Geography</option>
-                <option value="23">History</option>
-                <option value="24">Politics</option>
-                <option value="25">Art</option>
-                <option value="26">Celebrities</option>
-                <option value="27">Animals</option>
-                <option value="28">Vehicles</option>
-                <option value="29">Entertainment: Comics</option>
-                <option value="30">Science: Gadgets</option>
-                <option value="31">
-                  Entertainment: Japanese Anime &amp; Manga
-                </option>
-                <option value="32">
-                  Entertainment: Cartoon &amp; Animations
-                </option>
+                {categories.map(({ label, value }) => (
+                  <option value={value}>{label}</option>
+                ))}
               </Field>
 
               <label htmlFor="type">Type</label>
               <Field id="type" name="type" component="select">
-                <option value="any">Any Type</option>
-                <option value="multiple">Multiple Choice</option>
-                <option value="boolean">True / False</option>
+                {types.map(({ label, value }) => (
+                  <option value={value}>{label}</option>
+                ))}
               </Field>
               <button type="submit">Play game</button>
             </fieldset>
